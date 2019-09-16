9c2b28f3ea66884fed5af38bad6f933dc7ba47d5
Ngoc Le <letuanngoc129@gmail.com>
2019-09-16 13:34:35 +0300

    Initial Commit

---
+++ refMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  refMe
+//
+//  Created by Ngoc Le on 16.9.2019.
+//  Copyright © 2019 Coder's Life. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
