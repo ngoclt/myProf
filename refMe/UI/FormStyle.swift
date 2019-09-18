767b22db2790ac4f87163399737cfb66c3c88e00
Ngoc Le <letuanngoc129@gmail.com>
2019-09-18 12:12:15 +0300

    Added gitignore file

---
+++ refMe/UI/FormStyle.swift
@@ -0,0 +1,9 @@
+//
+//  FormStyle.swift
+//  refMe
+//
+//  Created by Ngoc Le on 17.9.2019.
+//  Copyright © 2019 Coder's Life. All rights reserved.
+//
+
+import Foundation
---
+++ refMe/UI/IconTextField.swift
@@ -0,0 +1,9 @@
+//
+//  IconTextField.swift
+//  refMe
+//
+//  Created by Ngoc Le on 16.9.2019.
+//  Copyright © 2019 Coder's Life. All rights reserved.
+//
+
+import Foundation
